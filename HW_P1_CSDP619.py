75581818f1604f18222d55e74b07566fcd459576
Arya Das <aryad1@umbc.edu>
2024-09-24 15:52:35 -0400

    Changes Made

--- HW_P1_CSDP619.py
+++ HW_P1_CSDP619.py
@@ -11,31 +11,22 @@ import numpy as np
 e = math.e
 print("e: ", e)
 
-# Sigmoid Function and Sigmoid Derivative
-x = 0 # Update as needed, or take user input
 def sigmoid(x):
     return 1 / (1 + e**(-x))
-##sigmoid = 1 / (1 + e**(-x))
-#print("Sigmoid: ", sigmoid)
-#sigmoid_derivative = sigmoid * (1 - sigmoid)
+
 def sigmoid_derivative(x):
     return sigmoid(x) * (1 - sigmoid(x))
-#print("Sigmoid Derivative: ", sigmoid_derivative)
 
 
 # Tanh Function and Tanh Derivative
-#tanh = (e**x - e**(-x)) / (e**x + e**(-x))
-#print("Tanh: ", tanh)
 def tanh(x):
     return (e**x - e**(-x)) / (e**x + e**(-x))
-#tanh_derivative = 1 - tanh**2
-#print("Tanh Derivative: ", tanh_derivative)
+
 def tanh_derivative(x):
     return 1 - tanh(x)**2
 
 
 # ReLU Function and ReLU Derivative
-# relu = max(0, x)
 def relu(x):
     return max(0, x)
 
@@ -64,9 +55,6 @@ def softmax(values):
     exp_values = np.exp(values)
     exp_values_sum = np.sum(exp_values)
     return exp_values/exp_values_sum
-# e ^ (x - max(x)) / sum(e^(x - max(x))
-# = e ^ x / (e ^ max(x) * sum(e ^ x / e ^ max(x)))
-# = e ^ x / sum(e ^ x)
 
 def softmax_gradient(values):
     return 
